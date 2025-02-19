9e5bdaca6a9e7c372f84d438089aa7b592e43fc5
MaxCostaCardosoIFRO <159456189+MaxCostaCardosoIFRO@users.noreply.github.com>
2025-02-19 09:47:28 -0400

    Delete api_libertadores directory

--- api_libertadores/servico/deletaCampeonato_servico.js
+++
@@ -1,9 +0,0 @@
-import pool from "./conexao";
-export async function deletaCampeonato(id) {
-const  conexao = await pool.getConnection();
-const  query = 'DELETE FROM campeonatos WHERE id = ?'
-const   [resposta] = await conexao.execute(query,[id])
-console.log(resposta)
-conexao.release();
-return resposta
-}
